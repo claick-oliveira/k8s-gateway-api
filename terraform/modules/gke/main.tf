25427b65febefc099a5b819d5f49d8da3d069b76
Claick Oliveira <claick.cao@gmail.com>
2023-02-16 17:42:47 +0000

    Added service account and controle roles for GKE nodes

--- terraform/modules/gke/main.tf
+++ terraform/modules/gke/main.tf
@@ -1,23 +1,43 @@
 // gke module
 data "google_client_config" "default" {}
 
+resource "google_service_account" "gke-sa" {
+  account_id   = "tf-gke-sa"
+  display_name = "Service Account For GKE ${var.cluster_name}"
+  project      = var.gcp_project_name
+}
+resource "google_project_iam_member" "artifactregistry-role" {
+  role    = "roles/artifactregistry.reader"
+  member  = "serviceAccount:${google_service_account.gke-sa.email}"
+  project = var.gcp_project_name
+}
+
+resource "google_project_iam_member" "node-service-account-role" {
+  role    = "roles/container.nodeServiceAccount"
+  member  = "serviceAccount:${google_service_account.gke-sa.email}"
+  project = var.gcp_project_name
+}
+
 module "gke" {
   source                     = "terraform-google-modules/kubernetes-engine/google"
   project_id                 = var.gcp_project_name
   name                       = var.cluster_name
   region                     = var.gcp_region
+  regional                   = false
   zones                      = var.gcp_zones
   network                    = var.vpc_name
   subnetwork                 = var.subnet_name
   ip_range_pods              = var.cidr_pods
   ip_range_services          = var.cidr_services
-  http_load_balancing        = false
+  http_load_balancing        = true
   network_policy             = false
   horizontal_pod_autoscaling = true
   filestore_csi_driver       = false
   enable_cost_allocation     = true
   gateway_api_channel        = var.gateway_api_channel
-  create_service_account     = true
+  create_service_account     = false
+  release_channel            = "REGULAR"
+
 
   node_pools = [
     {
@@ -36,7 +56,8 @@ module "gke" {
       auto_repair            = true
       auto_upgrade           = true
       preemptible            = false
-      initial_node_count     = 80
+      initial_node_count     = 1
+      service_account        = google_service_account.gke-sa.email
     },
   ]
 
@@ -47,7 +68,8 @@ module "gke" {
       "https://www.googleapis.com/auth/monitoring",
       "https://www.googleapis.com/auth/service.management.readonly",
       "https://www.googleapis.com/auth/servicecontrol",
-      "https://www.googleapis.com/auth/trace.append"
+      "https://www.googleapis.com/auth/trace.append",
+      "https://www.googleapis.com/auth/cloud-platform"
     ]
   }
 
