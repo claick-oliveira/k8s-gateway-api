08ffb9ecace67b2a463f7d0c900ab720e15fd80a
Claick Oliveira <claick.cao@gmail.com>
2023-02-28 18:26:51 +0000

    Added outputs

--- terraform/modules/gke/outputs.tf
+++ terraform/modules/gke/outputs.tf
@@ -4,6 +4,25 @@ output "kubernetes_endpoint" {
   value     = module.gke.endpoint
 }
 
+output "cluster_name" {
+  description = "Cluster name"
+  value       = module.gke.name
+}
+
+output "location" {
+  value = module.gke.location
+}
+
+output "master_kubernetes_version" {
+  description = "Kubernetes version of the master"
+  value       = module.gke.master_version
+}
+
+output "ca_certificate" {
+  description = "The cluster ca certificate (base64 encoded)"
+  value       = module.gke.ca_certificate
+}
+
 output "client_token" {
   sensitive = true
   value     = base64encode(data.google_client_config.default.access_token)
